dd0debfbb34d71a5d2e00b9840f3478f206ff4f4
FuzzyForest <red.hope6833@fastmail.com>
2023-09-15 21:26:54 +0100

    Day 13

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ use shallot_hashmap::HashMap;
 use shallot_strings::LispString;
 
 create_layer!(
-   over shallot_hashmap, shallot_strings
+   over shallot_regex shallot_strings shallot_hashmap
    | atoms
    | builtins
 );
